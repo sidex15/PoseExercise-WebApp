fbb65fd0160eb81cc8475e3b83b1e5b24c15bf09
Richard Dean Alforque <alforquerichard0611@gmail.com>
2023-05-02 12:20:11 +0800

    Add signup button on topbar

--- thesis_interface/src/components/Topbar.js
+++ thesis_interface/src/components/Topbar.js
@@ -1,6 +1,7 @@
 import { useRouter } from "next/router";
 import { useState } from 'react';
 import { RiLogoutBoxRLine } from "react-icons/ri";
+import { HiOutlineUserCircle } from "react-icons/hi2";
 import Cookies from 'js-cookie';
 const Topbar = () => {
     const router = useRouter();
@@ -11,13 +12,29 @@ const Topbar = () => {
         router.push('/login');
     }
 
+    const [isLogin , setIsLogin] = useState(true)
+
+
+    // if condition {
+    //     setIsLogin(false)
+    // }
+
+    const SignUp = (e) => {
+        e.preventDefault();
+        router.push('/signup');
+    }
+
 
     return ( 
         <div className='w-full'>
             <nav className="flex items-center justify-end flex-wrap bg-cyan-blue h-14">
-                <div className='text-white text-sm flex items-center gap-x-2 cursor-pointer p-3 hover:bg-light-white rounded-md mr-4' onClick={Backlogin}>
+                <div className={`${ isLogin ? 'flex' : 'hidden' } text-white text-sm items-center gap-x-2 cursor-pointer p-3 hover:bg-light-white rounded-md mr-4`} onClick={Backlogin}>
                     <RiLogoutBoxRLine size="28px" color="white"/>
                     <span>Logout</span>
+                </div>
+                <div className={`${ isLogin ? 'hidden' : 'flex' } text-white text-sm items-center gap-x-2 cursor-pointer p-3 hover:bg-light-white rounded-md mr-4`} onClick={SignUp}>
+                    <HiOutlineUserCircle size="28px" color="white"/>
+                    <span>Sign Up</span>
                 </div>
             </nav>
         </div>
