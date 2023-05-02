0aa6c767df08b5e294eed6cc3d2e952130bc69ab
Richard Dean Alforque <alforquerichard0611@gmail.com>
2023-05-03 01:03:19 +0800

    Set the landing page to dashboard

--- thesis_interface/src/pages/index.js
+++ thesis_interface/src/pages/index.js
@@ -1,32 +1,40 @@
-import Login from "./login";
-import Cookies from 'js-cookie';
-import { useRouter } from 'next/navigation';
-import { useEffect, useState } from 'react';
+// import Login from "./login";
+// import Cookies from 'js-cookie';
+// import { useRouter } from 'next/navigation';
+// import { useEffect, useState } from 'react';
 
-function Routes() {
-  const router = useRouter();
-  const token = Cookies.get('token');
-  useEffect(() => {
-    const verifyToken = async () => {
-        try {
-            const res = await fetch('/api/verify-token', {
-                headers: {
-                    Authorization: `Bearer ${token}`
-                }
-            });
+// function Routes() {
+//   const router = useRouter();
+//   const token = Cookies.get('token');
+//   useEffect(() => {
+//     const verifyToken = async () => {
+//         try {
+//             const res = await fetch('/api/verify-token', {
+//                 headers: {
+//                     Authorization: `Bearer ${token}`
+//                 }
+//             });
 
-            if (!res.ok) {
-                const { message } = await res.json();
-                throw new Error(message);
-            }
-          router.push('/dashboard');
-        } catch (error) {
-          return (<Login />);
-        }
-    }
-    verifyToken();
-  }, []);
+//             if (!res.ok) {
+//                 const { message } = await res.json();
+//                 throw new Error(message);
+//             }
+//           router.push('/dashboard');
+//         } catch (error) {
+//           return (<Login />);
+//         }
+//     }
+//     verifyToken();
+//   }, []);
   
-}
+// }
 
-export default Routes;
+// export default Routes;
+
+import Dashboard from "./dashboard";
+
+export default function Home() {
+  return (
+    <Dashboard />
+  )
+}
